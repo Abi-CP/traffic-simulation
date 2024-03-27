a35fec296d99bbde479c60f2f6ef0961e2e4cd3f
Abi-CP <c.p.abishek@gmail.com>
2024-03-28 00:16:06 +0530

    countdown fixed

--- script.js
+++ script.js
@@ -7,15 +7,12 @@ const ts1 = document.getElementById('ts1')
 const ts2 = document.getElementById('ts2')
 const ts3 = document.getElementById('ts3')
 
-// const redLight = document.querySelector('.frame .redLight')
 var signal
 var redLight
 var yellowLight
 var greenLight
 var trafficPassed = false
 var signalState = [0, 0, 0, 0]
-
-// console.log(ts0.redLight)
 
 var prevVehicle = new Array(8)
 
@@ -72,9 +69,6 @@ function createVehicle(mode) {
   img.style.transform = transform
   trackElem.appendChild(img)
 
-  // console.log(prevVehicle[trackId] != undefined)
-  //   console.log(img.id + ' ' + direction + ' ' + source+' '+prevVehicle[trackId])
-
   let duration = 15000 + vehicleType * 0.2 * 2000
   animateVehicle(img.id, vehicleType, direction, prevVehicle[trackId], duration, trafficPassed)
   prevVehicle[trackId] = img.id
@@ -84,141 +78,127 @@ function slowDown(id, vehicleType, direction, prevVehicleId, remainingTime, traf
   animateVehicle(id, vehicleType, direction, prevVehicleId, remainingTime > 12500 ? 12500 : remainingTime + 10, trafficPassed)
 }
 
-function trafficController() {
-  let i = 0
-  switchSignals(i++)
+async function switchSignals(currSignalIndex, nextSignalIndex) {
+  let currSC = document.getElementById(`tsc${currSignalIndex}`)
+  let nextSC = document.getElementById(`tsc${nextSignalIndex}`)
 
-  setInterval(() => {
-    switchSignals(i)
-    i = (i + 1) % 4 // Increment i and ensure it wraps around from 3 back to 0
-  }, 18000)
-}
+  var greenSC = 10
+  var yellowSC = 5
+  var readySC = 10
 
-function switchSignals(i) {
-  var currCounter = document.getElementById(`tsc${i}`);
-  var readyCounter = document.getElementById(`tsc${(i+1)%4}`);
-  var readyCount = 10,
-    goCount = 10,
-    slowCount = 4;
+  enableGreen(currSignalIndex)
+  currSC.innerHTML = greenSC--
 
-  setTimeout(() => {
-    signalState[i] = 1;
-    var goInterval = setInterval(() => {
-      if (goCount > -1) {
-        currCounter.innerText = goCount;
-        goCount--;
+  function startGreenInterval() {
+    enableGreen(currSignalIndex)
+    let greenInterval = setInterval(() => {
+      if (greenSC >= 0) {
+        currSC.innerHTML = greenSC--
+
+        if (greenSC < 3) {
+          nextSC.innerHTML = readySC--
+        }
       } else {
-        currCounter.innerText = '5';
-        clearInterval(goInterval);
+        currSC.innerHTML = '--'
+        disableGreen(currSignalIndex)
+        clearInterval(greenInterval)
+        startYellowInterval()
+        continueReadyInterval()
       }
-    }, 999);
-    enableGreen(i);
-    // checkTraffic(signalState) // Pass the signalState array to checkTraffic
-  }, 0);
+    }, 1000)
+  }
 
-  setTimeout(() => {
-    var readyInterval = setInterval(() => {
-      if (readyCount > -1) { // Correct condition for countdown
-        readyCounter.innerText = readyCount;
-        readyCount--;
+  function startYellowInterval() {
+    enableYellow(currSignalIndex)
+    let yellowInterval = setInterval(() => {
+      if (yellowSC >= 0) {
+        currSC.innerHTML = yellowSC--
       } else {
-        currCounter.innerText = '10';
-        clearInterval(readyInterval);
+        console.log('toRed')
+        currSC.innerHTML = '--'
+        disableYellow(currSignalIndex)
+        enableRed(currSignalIndex)
+        clearInterval(yellowInterval)
       }
-    }, 999);
-  }, 5999);
+    }, 1000)
+  }
 
-  setTimeout(() => {
-    signalState[i] = 2;
-    var slowInterval = setInterval(() => {
-      if (slowCount > -1) {
-        currCounter.innerText = slowCount;
-        slowCount--;
-        console.log(slowCount);
+  async function continueReadyInterval() {
+    nextSC.innerHTML = readySC--
+    let readyInterval = setInterval(() => {
+      if (readySC >= 0) {
+        nextSC.innerHTML = readySC--
       } else {
-        currCounter.innerText = '--';
-        clearInterval(slowInterval);
+        nextSC.innerHTML = '--'
+        clearInterval(readyInterval)
+        switchSignals(nextSignalIndex, (nextSignalIndex + 1) % 4) // Assuming 4 signals in total
       }
-    }, 1000);
-    enableYellow(i);
-    // checkTraffic(signalState) // Pass the signalState array to checkTraffic
-  }, 11999);
+    }, 1000)
+  }
 
-  setTimeout(() => {
-    signalState[i] = 0; // Update signal state to red
-    enableRed(i);
-    // console.log(signalState)
-    // checkTraffic(signalState) // Pass the signalState array to checkTraffic
-  }, 17999);
+  function disableRed(i) {
+    signal = document.getElementById(`ts${i}`)
+    redLight = signal.querySelector('.frame .light.red')
+    redLight.style.backgroundColor = '#7e7e7e'
+    signalState[i] = -1
+  }
+
+  function disableYellow(i) {
+    signal = document.getElementById(`ts${i}`)
+    yellowLight = signal.querySelector('.frame .light.yellow')
+    yellowLight.style.backgroundColor = '#7e7e7e'
+    signalState[i] = -1
+  }
+
+  function disableGreen(i) {
+    signal = document.getElementById(`ts${i}`)
+    greenLight = signal.querySelector('.frame .light.green')
+    greenLight.style.backgroundColor = '#7e7e7e'
+    signalState[i] = -1
+  }
+
+  function enableRed(i) {
+    console.log('red')
+    signal = document.getElementById(`ts${i}`)
+    disableYellow(i)
+    redLight = signal.querySelector('.frame .light.red')
+    redLight.style.backgroundColor = 'red'
+    signalState[i] = 0
+  }
+
+  function enableYellow(i) {
+    signal = document.getElementById(`ts${i}`)
+    disableGreen(i)
+    yellowLight = signal.querySelector('.frame .light.yellow')
+    yellowLight.style.backgroundColor = 'yellow'
+    signalState[i] = 2
+  }
+
+  function enableGreen(i) {
+    signal = document.getElementById(`ts${i}`)
+    disableRed(i)
+    greenLight = signal.querySelector('.frame .light.green')
+    greenLight.style.backgroundColor = '#39d600'
+    signalState[i] = 1
+  }
+
+  startGreenInterval()
 }
 
-
-trafficController()
-
-function disableRed(i) {
-  signal = document.getElementById(`ts${i}`)
-  redLight = signal.querySelector('.frame .light.red')
-  redLight.style.backgroundColor = '#7e7e7e'
-  signalState[i] = -1
-}
-
-function disableYellow(i) {
-  signal = document.getElementById(`ts${i}`)
-  yellowLight = signal.querySelector('.frame .light.yellow')
-  yellowLight.style.backgroundColor = '#7e7e7e'
-  signalState[i] = -1
-}
-
-function disableGreen(i) {
-  signal = document.getElementById(`ts${i}`)
-  greenLight = signal.querySelector('.frame .light.green')
-  greenLight.style.backgroundColor = '#7e7e7e'
-  signalState[i] = -1
-}
-
-function enableRed(i) {
-  signal = document.getElementById(`ts${i}`)
-  disableYellow(i)
-  redLight = signal.querySelector('.frame .light.red')
-  redLight.style.backgroundColor = 'red'
-  signalState[i] = 0
-}
-
-function enableYellow(i) {
-  signal = document.getElementById(`ts${i}`)
-  //   disableRed(i)
-  disableGreen(i)
-  yellowLight = signal.querySelector('.frame .light.yellow')
-  yellowLight.style.backgroundColor = 'yellow'
-  signalState[i] = 2
-}
-
-function enableGreen(i) {
-  signal = document.getElementById(`ts${i}`)
-  disableRed(i)
-  greenLight = signal.querySelector('.frame .light.green')
-  greenLight.style.backgroundColor = '#39d600'
-  signalState[i] = 1
-}
+switchSignals(0, 1)
 
 async function checkTraffic(id, vehicleType, direction, prevVehicleId, remainingTime, trafficPassed, signal) {
-  // console.log(id);
   let currentSignal = getSignalState(signal)
   if (currentSignal === 1) {
-    // Check if the signal is green
     trafficPassed = true
-    // console.log(signal);
     animateVehicle(id, vehicleType, direction, prevVehicleId, 9000 + vehicleType * 0.2 * 2000, trafficPassed)
   }
   animateVehicle(id, vehicleType, direction, prevVehicleId, 9000 + vehicleType * 0.2 * 2000, trafficPassed)
-
-  // }
 
   setTimeout(() => {}, 1)
 }
 
 function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, trafficPassed) {
-  // console.log(id);
   const v = document.querySelector(`#${id}`)
   let initialPosition
   let targetPosition
@@ -258,8 +238,6 @@ function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, tra
 
     if (progress >= 1) {
       v.style.transform = targetTransform
-      // console.log('Animation complete. Current position:', targetPosition)
-      v.remove()
       return
     }
 
@@ -270,12 +248,10 @@ function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, tra
     switch (direction) {
       case 'up':
         if (!trafficPassed && v.getBoundingClientRect().y - mapOffsetY < 448) {
-          // console.log(id);
-
           checkTraffic(id, vehicleType, direction, prevVehicleId, duration + 10, trafficPassed, 0)
           return
         }
-        if (trafficPassed && v.getBoundingClientRect().y + v.clientHeight - mapOffsetY < 10) {
+        if (trafficPassed && v.getBoundingClientRect().y - mapOffsetY < -30) {
           v.remove()
         }
         break
@@ -285,7 +261,7 @@ function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, tra
           checkTraffic(id, vehicleType, direction, prevVehicleId, duration - elapsedTime + 10, trafficPassed, 2)
           return
         }
-        if (trafficPassed && v.getBoundingClientRect().y + v.clientHeight - mapOffsetY > 778) {
+        if (trafficPassed && v.getBoundingClientRect().y + v.clientHeight - mapOffsetY > 808) {
           v.remove()
         }
         break
@@ -295,7 +271,7 @@ function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, tra
           checkTraffic(id, vehicleType, direction, prevVehicleId, duration - elapsedTime + 10, trafficPassed, 3)
           return
         }
-        if (trafficPassed && v.getBoundingClientRect().x - v.clientWidth - mapOffsetX < 10) {
+        if (trafficPassed && v.getBoundingClientRect().x - mapOffsetX < -30) {
           v.remove()
         }
         break
@@ -304,7 +280,7 @@ function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, tra
           checkTraffic(id, vehicleType, direction, prevVehicleId, duration - elapsedTime + 10, trafficPassed, 1)
           return
         }
-        if (trafficPassed && v.getBoundingClientRect().x + v.clientWidth - mapOffsetX > 778) {
+        if (trafficPassed && v.getBoundingClientRect().x + v.clientWidth - mapOffsetX > 808) {
           v.remove()
         }
         break
@@ -322,11 +298,7 @@ function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, tra
           ? prevVehicle.getBoundingClientRect().x - v.getBoundingClientRect().x - v.clientWidth
           : null
 
-      //   const distance = Math.abs(prevPosition - currentPos)
-
-      // const position
       if (distance < 10) {
-        // console.log('Animation Paused. Current position:', targetPosition)
         if (trafficPassed) {
           slowDown(id, vehicleType, direction, prevVehicleId, duration - elapsedTime + 10, trafficPassed)
         } else {
@@ -334,30 +306,23 @@ function animateVehicle(id, vehicleType, direction, prevVehicleId, duration, tra
         }
         return
       }
-
-      //   console.log(prevVehicleId, ' ', prevPosition, ' ', id, ' ', newPosition)
     }
 
-    // Continue the animation if it was paused and distance is greater than 100 pixels
     if (direction == 'up' || direction == 'down') {
       v.style.transform = `translateY(${newPosition}px)`
     } else if (direction == 'right' || direction == 'left') {
       v.style.transform = `translateX(${newPosition}px)`
     }
 
-    // Continue the animation loop regardless of whether it was paused or not
     animationId = requestAnimationFrame(updatePosition)
   }
 
   animationId = requestAnimationFrame(updatePosition)
 }
 
-// // Start the animation loop
-// animateVehicle(); // You need to call this function somewhere to start the animation loop
-
-// Call the function to start the animation
-// animateElement();
-
+createVehicle(0)
+createVehicle(0)
+createVehicle(0)
 createVehicle(0)
 // createVehicle(0)
 // createVehicle(0)
@@ -368,12 +333,14 @@ createVehicle(0)
 // createVehicle(0)
 // createVehicle(0)
 // createVehicle(0)
-// // createVehicle(0)
-// // createVehicle(0)
-// // createVehicle(0)
-// // createVehicle(0)
-
 // createVehicle(0)
-// setInterval(() => {
-//   createVehicle(0)
-// }, 5000)
+
+createVehicle(0)
+setInterval(() => {
+  createVehicle(0)
+}, 6000)
+
+createVehicle(0)
+setInterval(() => {
+  createVehicle(0)
+}, 15000)
